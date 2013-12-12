74c55c68afdf80e199e7d2c92358d7b65a8fce8d
RayPatterson <ray.patterson@akqa.com>
2013-12-12 14:32:37 -0800

    Adding files from source subtree branch.

---
+++ source/assets/watch/project/modules/video-player/scripts.js
@@ -0,0 +1,21 @@
+//= require_tree ./
+;
+(function(win) {
+
+  var site_namespace = win.__site_namespace__;
+
+  var namespace = site_namespace + '.Modules.VideoPlayer';
+
+  var logger = Logger.get(namespace);
+
+  var App = Namespace(site_namespace + '.App');
+  var Models = Namespace(site_namespace + '.Models');
+  var YouTube = Namespace(site_namespace + '.Media.YouTube');
+
+  var $videoPlayerTarget = $(".video-player-target");
+
+  Models.videoCollection.on('selected', function(model) {
+    YouTube.createPlayer($videoPlayerTarget, model.get('video_id'));
+  });
+
+}(window));
