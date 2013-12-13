42f71091b8c38bdce18e589cefefd09ebc59746f
RayPatterson <ray.patterson@akqa.com>
2013-12-12 16:09:43 -0800

    Squashed 'source/' content from commit 64580f8
    git-subtree-dir: source
    git-subtree-split: 64580f8b8d6fcbbb18bbda1e848c6efded16b8c6

---
+++ assets/watch/project/models/video-collection.js
@@ -0,0 +1,34 @@
+(function(win) {
+
+  var site_namespace = win.__site_namespace__;
+
+  var Main = Namespace(site_namespace + '.Main');
+  var App = Namespace(site_namespace + '.App');
+  var Models = Namespace(site_namespace + '.Models');
+
+  var Model = Backbone.Model.extend({
+    initialize: function() {
+      var selectable = new Backbone.Picky.Selectable(this);
+      _.extend(this, selectable);
+    }
+  });
+
+  var Collection = Backbone.Collection.extend({
+    comparator: function(model) {
+      var index = this.indexOf(model);
+      model.set('index', index);
+      model.set('episode_id', (index + 1)
+        .toString());
+      return model;
+    },
+    initialize: function() {
+      var singleSelect = new Backbone.Picky.SingleSelect(this);
+      _.extend(this, singleSelect);
+    },
+    model: Model
+  });
+
+  // Expose model
+  Models.videoCollection = new Collection();
+
+}(window));
