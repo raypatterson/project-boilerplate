ebfdc04efef0a7c58159cb9fb3ae7a7a618871dd
RayPatterson <ray.patterson@akqa.com>
2013-12-12 23:26:41 -0800

    Squashed 'source/' content from commit 8be5d41
    git-subtree-dir: source
    git-subtree-split: 8be5d4112452148f2db2d89fb32e84c2776ec01e

---
+++ assets/watch/_project.js
@@ -0,0 +1,6 @@
+// --------------------------------------------------------------
+// Project
+// --------------------------------------------------------------
+//= require project/app/config
+//= require project/app/init
+//= require project/app/start
