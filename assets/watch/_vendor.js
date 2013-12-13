11b292c2f38cbdd98527149ca106df03747dac8b
RayPatterson <ray.patterson@akqa.com>
2013-12-12 23:28:42 -0800

    Squashed 'source/' content from commit 64580f8
    git-subtree-dir: source
    git-subtree-split: 64580f8b8d6fcbbb18bbda1e848c6efded16b8c6

---
+++ assets/watch/_vendor.js
@@ -0,0 +1,19 @@
+// --------------------------------------------------------------
+// Bower
+// --------------------------------------------------------------
+//= require modernizr/modernizr
+//= require handlebars/handlebars.runtime
+//= require jquery/jquery
+//= require lodash/dist/lodash.underscore
+//= require backbone/backbone
+//= require backbone.marionette/lib/backbone.marionette
+// --------------------------------------------------------------
+// Vendor
+//
+// NOTE: Dependencies must be declared before 'require_tree'
+//
+// --------------------------------------------------------------
+//= require vendor/backbone/backbone.picky
+//= require vendor/util/logger
+//= require vendor/util/kwerystring
+//= require vendor/util/Namespace
